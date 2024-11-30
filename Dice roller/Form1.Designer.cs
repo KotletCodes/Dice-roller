f439c429098313e3f43f3c89b9f56830470cd168
przem <przem@JLLAPTOP>
2024-11-30 22:12:44 +0000

    Add project files.

---
+++ Dice roller/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Dice_roller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Location = new System.Drawing.Point(396, 490);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 76);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.Location = new System.Drawing.Point(24, 190);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 76);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "d4";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.Location = new System.Drawing.Point(210, 190);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 76);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "d6";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.Location = new System.Drawing.Point(396, 190);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(180, 76);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "d8";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.Location = new System.Drawing.Point(582, 190);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(180, 76);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "d10";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button6.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.Location = new System.Drawing.Point(768, 190);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(180, 76);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "d12";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(396, 397);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 22);
+            this.textBox1.TabIndex = 16;
+            this.textBox1.Text = "number of dice";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(410, 439);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(154, 25);
+            this.button7.TabIndex = 17;
+            this.button7.Text = "enter";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button8.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.Location = new System.Drawing.Point(396, 67);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(180, 76);
+            this.button8.TabIndex = 18;
+            this.button8.Text = "d20";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button9.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.button9.FlatAppearance.BorderSize = 0;
+            this.button9.Location = new System.Drawing.Point(396, 283);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(180, 76);
+            this.button9.TabIndex = 19;
+            this.button9.Text = "d100";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(982, 578);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Dice roller/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dice_roller
+{
+    public partial class Form1 : Form
+    {
+        public string mod { get; set; }
+        public string num { get; set; }
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            this.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string number = textBox1.Text;
+            num = number;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+           string mody = textBox1.Text;
+            mod = mody;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int modifier = Convert.ToInt32(mod);
+            int repeats = Convert.ToInt32(num);
+            int sum = 0;
+            for (int x = 0; x < repeats; x++)
+            {
+                sum = sum + rand.Next(1, 5);
+            
+            
+            }
+            sum = sum + modifier;
+            MessageBox.Show(sum + " is your total roll");
+            
+
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int modifier = Convert.ToInt32(mod);
+            int repeats = Convert.ToInt32(num);
+            int sum = 0;
+            for (int x = 0; x < repeats; x++)
+            {
+                sum = sum + rand.Next(1, 7);
+
+
+            }
+            sum = sum + modifier;
+            MessageBox.Show(sum + " is your total roll");
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            int modifier = Convert.ToInt32(mod);
+            int repeats = Convert.ToInt32(num);
+            int sum = 0;
+            for (int x = 0; x < repeats; x++)
+            {
+                sum = sum + rand.Next(1, 9);
+
+
+            }
+            sum = sum + modifier;
+            MessageBox.Show(sum + " is your total roll");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int modifier = Convert.ToInt32(mod);
+            int repeats = Convert.ToInt32(num);
+            int sum = 0;
+            for (int x = 0; x < repeats; x++)
+            {
+                sum = sum + rand.Next(1, 11);
+
+
+            }
+            sum = sum + modifier;
+            MessageBox.Show(sum + " is your total roll");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int modifier = Convert.ToInt32(mod);
+            int repeats = Convert.ToInt32(num);
+            int sum = 0;
+            for (int x = 0; x < repeats; x++)
+            {
+                sum = sum + rand.Next(1, 13);
+
+
+            }
+            sum = sum + modifier;
+            MessageBox.Show(sum + " is your total roll");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int modifier = Convert.ToInt32(mod);
+            int repeats = Convert.ToInt32(num);
+            int sum = 0;
+            int roll = 0;
+            for (int x = 0; x < repeats; x++)
+            {
+                roll = rand.Next(1, 21);
+                sum = sum + roll;
+                
+                
+
+
+            }
+            sum = sum + modifier;
+            MessageBox.Show(sum + " is your total roll");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int modifier = Convert.ToInt32(mod);
+            int repeats = Convert.ToInt32(num);
+            int sum = 0;
+            for (int x = 0; x < repeats; x++)
+            {
+                sum = sum + rand.Next(1, 101);
+
+
+            }
+            sum = sum + modifier;
+            MessageBox.Show(sum + " is your total roll");
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+   
+}
